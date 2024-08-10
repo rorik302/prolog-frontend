fa8fd755ecc35d13834d980af4c6a9ed1644e338
Aleksandr Danilenko <rorik302@gmail.com>
2024-08-10 18:11:15 +0300

    chore: добавлены layouts

---
+++ layouts/auth.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-container class="auth-layout">
+    <slot />
+  </el-container>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.auth-layout {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: var(--el-fill-color);
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <el-container class="default-layout">
+    <slot />
+  </el-container>
+</template>
+
+<script setup lang="ts"></script>
